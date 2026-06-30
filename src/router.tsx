b53ed7e22e8e3fb3d22dcc474d3966bf2e3bb714
VoGiaHuy <huyvog6226@gmail.com>
2026-06-30 17:58:51 +0700

    testing

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,12 @@
 import { createBrowserRouter } from "react-router-dom"
 
+import { AdminPage } from "@/pages/admin-page"
 import { HomePage } from "@/pages/home-page"
-import { LoginPage } from "./pages/login-page"
-import { UsersPage } from "./pages/user-page"
+import {LoginPage} from "@/pages/login-page"
+import { AttendancePage } from "@/pages/attendance-page"
+// import { UsersPage } from "./pages/user-page"
+import { EmployeeDetail } from "./pages/employee-detail"
+import { CreateEmployee } from "./pages/create-employee"
 
 /**
  * App router — React Router v7 data API (`createBrowserRouter`).
@@ -16,10 +20,13 @@ import { UsersPage } from "./pages/user-page"
  * auth, and use loaders if you want route-level data fetching.
  */
 export const router = createBrowserRouter([
-  { path: "/", element: <HomePage />, children: [
-      { path: "/login", element: <LoginPage /> },
-      { path: "/users", element: <UsersPage /> },
-  ] },
-
-  // { path: "/users/:id", element: <UserDetailPage /> },
+  { path: "/", element: <HomePage /> },
+  { path: "/login", element: <LoginPage/> },
+  { path: "/attendance", element: <AttendancePage /> },
+  { path: "/admin", element: <AdminPage /> },
+  { path: "/admin/employees/create", element: <CreateEmployee /> },
+  { path: "/admin/employees/:id", element: <EmployeeDetail/>},
+  // { path: "/users", element: <UsersPage /> },
+  { path: "/employees/:id", element: <EmployeeDetail/>},
+  { path: "/employees/", element: <CreateEmployee /> },
 ])
